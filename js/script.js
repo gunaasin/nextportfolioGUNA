28f52eab67bd78d8d0b4f62ce5dfa73787b98ad8
dineshdiv <gunatechy@gmail.com>
2024-04-27 23:11:44 +0530

    17

--- js/script.js
+++ js/script.js
@@ -1,12 +1,38 @@
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+ScrollTrigger.normalizeScroll(true)
+let smoother = ScrollSmoother.create({
+  smooth: 2,
+  effects: true,
+  normalizeScroll: true
+});
+
+ScrollTrigger.create({
+  trigger: ".box-c",
+  pin: true,
+  start: "center center",
+//   markers: true
+});
+
+
+// document.querySelector("projectbut").addEventListener("click", e => {
+//     smoother.scrollTo(".box-c", true, "center center");
+    
+//     // gsap.to(smoother, {
+//     // 	scrollTop: smoother.offset(".box-c", "center center"),
+//     // 	duration: 1
+//     // });
+//   });
+  
+
 document.addEventListener("DOMContentLoaded", function() {
   const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
           if (entry.isIntersecting) {
               entry.target.classList.add('animate__slideInUp');
-              entry.target.classList.remove('animate__fadeOutDown');
+            //   entry.target.classList.remove('animate__fadeOutDown');
           } else {
-              entry.target.classList.remove('animate__slideInUp');
-              entry.target.classList.add('animate__fadeOutDown');
+            //   entry.target.classList.remove('animate__slideInUp');
+            //   entry.target.classList.add('animate__fadeOutDown');
           }
       });
   });
@@ -31,3 +57,38 @@ document.addEventListener("DOMContentLoaded", function() {
       observers.observe(el);
   });
 });
+
+
+
+// ////////
+
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+// Close navbar when link is clicked
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
+
+
+
+let myDate = document.querySelector("#datee");
+
+const yes = new Date().getFullYear();
+myDate.innerHTML = yes;
+
+
+
